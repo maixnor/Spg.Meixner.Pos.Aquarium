2b4bea7f3bb04a7d3571bec6ff51e6dd14e4aae0
Benjamin Meixner <mei18930@spengergasse.at>
2021-11-04 17:46:44 +0100

    basic setup for angular

---
+++ Spg.Meixner.Pos.Aquarium.Angular/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Spg.Meixner.Pos.Aquarium.Angular.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spg.Meixner.Pos.Aquarium.Angular.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
